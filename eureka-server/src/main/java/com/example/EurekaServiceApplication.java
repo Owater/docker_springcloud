fbb5b60ba3ec268f90ff833f59986ed67414ab9e
Owater <1006340261@qq.com>
2017-08-04 22:25:09 +0800

    rename

--- eureka-server/src/main/java/com/example/EurekaServiceApplication.java
+++ eureka-server/src/main/java/com/example/EurekaServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringCloudDemoApplication {
+public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudDemoApplication.class, args);
+		SpringApplication.run(EurekaServiceApplication.class, args);
 	}
 }
--- eureka-server/src/test/java/com/example/EurekaServiceApplicationTests.java
+++ eureka-server/src/test/java/com/example/EurekaServiceApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class SpringCloudDemoApplicationTests {
+public class EurekaServiceApplicationTests {
 
 	@LocalServerPort
 	private int port;
