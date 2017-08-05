91db6234af668bf6139e8e12df602b335fd19909
Owater <1006340261@qq.com>
2017-08-05 21:26:56 +0800

    add hystrix

---
+++ hystrix-dashboard/src/main/java/com/owater/HystrixDashboardApplication.java
@@ -0,0 +1,19 @@
+package com.owater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbine
+@EnableEurekaClient
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
